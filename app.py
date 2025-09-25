9fb9627776689fcf6a88fa6adef35e213b809d83
Alexandre Oliveira <alexprofi131@gmail.com>
2025-09-25 17:36:55 -0300

    app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 from Views import *
 from Utilidade import *
 from Models import *
-from Menu import menu_com_senha, principal_menu
+from Menu import *
 from tests.testes import groqCloud
 
 menu_com_senha.showMenuComSenha()
+
 # principal_menu.showPrincipal()
