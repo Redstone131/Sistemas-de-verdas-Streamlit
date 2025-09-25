17d0940fdc646954f29694e150b72ede1858c1e4
Alexandre Oliveira <alexprofi131@gmail.com>
2025-09-25 17:44:58 -0300

    app.py

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from Views import *
 from Utilidade import *
 from Models import *
 from Menu.menu_com_senha import showSenha
+from Menu.principal_menu import showPrincipal
 from tests.testes import groqCloud
 
 showSenha()
 
 # principal_menu.showPrincipal()
+
 
 
 
